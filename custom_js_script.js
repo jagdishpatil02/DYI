42a9479dfd1368a2fa24f68642de40a64a948a0c
Jagdish Patil <patil.jagdish222@gmail.com>
2023-05-13 16:09:58 +0530

    added solution for svg icons and css for ui

--- custom_js_script.js
+++ custom_js_script.js
@@ -8,25 +8,31 @@ if (enabled == "true") {
   const newTextcolor = "#fff";
   // Loop through all stylesheets on the page
   changeColor(newBgColor, newTextcolor);
+  const svgColor = "#fff";
+  changeSVGIcons(svgColor);
+} else {
+  console.log("---------------");
+  // Loop through all elements on the page
+  const newBgColor = "#fff"; // Replace with your new background color
+  const newTextcolor = "#000";
+  const svgColor = "#000";
+  changeSVGIcons(svgColor);
+  // Loop through all stylesheets on the page
+  changeColor(newBgColor, newTextcolor);
+}
+
+function changeSVGIcons(svgColor) {
   // Get all SVG elements in the DOM
   const svgs = document.getElementsByTagName("svg");
-
   // Loop through each SVG element
   for (let i = 0; i < svgs.length; i++) {
     // Get all path elements inside the SVG
     const paths = svgs[i].getElementsByTagName("path");
     // Loop through each path element and apply the white stroke color
     for (let j = 0; j < paths.length; j++) {
-      paths[j].setAttribute("stroke", "#FFFFFF"); // Set the stroke color to white
+      paths[j].setAttribute("stroke", svgColor); // Set the stroke color to white
     }
   }
-} else {
-  console.log("---------------");
-  // Loop through all elements on the page
-  const newBgColor = "#fff"; // Replace with your new background color
-  const newTextcolor = "#000";
-  // Loop through all stylesheets on the page
-  changeColor(newBgColor, newTextcolor);
 }
 
 function changeColor(newBgColor, newTextcolor) {
@@ -41,10 +47,12 @@ function changeColor(newBgColor, newTextcolor) {
 
       // Check if the rule has a background color property
       if (rule.style && rule.style.backgroundColor) {
+        oldBgColor = rule.style.backgroundColor;
         // Replace the rule's background color with the new color
         rule.style.backgroundColor = newBgColor;
       }
       if (rule.style && rule.style.color) {
+        oldTextColor = rule.style.color;
         // Replace the rule's background color with the new color
         rule.style.color = newTextcolor;
       }
