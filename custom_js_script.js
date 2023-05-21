90fb6d1e73309a2e4d74927a4023d7d2c56f1b96
Jagdish Patil <patil.jagdish222@gmail.com>
2023-05-21 12:21:34 +0530

    code optimization changes

--- custom_js_script.js
+++ custom_js_script.js
@@ -1,17 +1,19 @@
 var enabled = window.localStorage.getItem("enabled"); //default
 
 if (enabled == "true") {
+  const rootElement = document.documentElement;
+  const bodyElement = document.body;
   // Check if the user prefers a light or dark color scheme
   if (
     document.body.hasAttribute("data-theme") ||
     document.documentElement.hasAttribute("data-theme") ||
     document.documentElement.hasAttribute("data-dark-theme")
   ) {
-    document.documentElement.style.setProperty("--color-scheme", "dark");
-    document.documentElement.style.setProperty("color-scheme", "dark");
-    document.documentElement.setAttribute("data-color-mode", "dark");
-    document.documentElement.setAttribute("data-theme", "dark");
-    document.body.setAttribute("data-theme", "dark");
+    rootElement.style.setProperty("--color-scheme", "dark");
+    rootElement.style.setProperty("color-scheme", "dark");
+    rootElement.setAttribute("data-color-mode", "dark");
+    rootElement.setAttribute("data-theme", "dark");
+    bodyElement.setAttribute("data-theme", "dark");
   } else {
     const newBgColor = "#181a1b"; // Replace with your new background color
     const newTextcolor = "#fff";
@@ -21,16 +23,18 @@ if (enabled == "true") {
     changeSVGIcons(svgColor);
   }
 } else {
+  const rootElement = document.documentElement;
+  const bodyElement = document.body;
   if (
     document.body.hasAttribute("data-theme") ||
     document.documentElement.hasAttribute("data-theme") ||
     document.documentElement.hasAttribute("data-dark-theme")
   ) {
-    document.documentElement.style.setProperty("--color-scheme", "light");
-    document.documentElement.style.setProperty("color-scheme", "light");
-    document.documentElement.setAttribute("data-color-mode", "light");
-    document.documentElement.setAttribute("data-theme", "light");
-    document.body.setAttribute("data-theme", "light");
+    rootElement.style.setProperty("--color-scheme", "light");
+    rootElement.style.setProperty("color-scheme", "light");
+    rootElement.setAttribute("data-color-mode", "light");
+    rootElement.setAttribute("data-theme", "light");
+    bodyElement.setAttribute("data-theme", "light");
   } else {
     const newBgColor = "#fff"; // Replace with your new background color
     const newTextcolor = "#000";
@@ -42,37 +46,29 @@ if (enabled == "true") {
 }
 function changeSVGIcons(svgColor) {
   // Get all SVG elements in the DOM
-  const svgs = document.getElementsByTagName("svg");
+  const svgs = document.querySelectorAll("svg");
   // Loop through each SVG element
   for (let i = 0; i < svgs.length; i++) {
     // Get all path elements inside the SVG
-    const paths = svgs[i].getElementsByTagName("path");
+    const paths = svgs[i].querySelectorAll("path");
     // Loop through each path element and apply the white stroke color
-    for (let j = 0; j < paths.length; j++) {
-      paths[j].setAttribute("stroke", svgColor); // Set the stroke color to white
+    for (const path of paths) {
+      path.setAttribute("stroke", svgColor);
     }
   }
 }
 
 function changeColor(newBgColor, newTextcolor) {
   const styleSheets = document.styleSheets;
-  for (let i = 0; i < styleSheets.length; i++) {
-    const styleSheet = styleSheets[i];
 
-    // Loop through all rules in the stylesheet
+  for (const styleSheet of styleSheets) {
     const rules = styleSheet.rules || styleSheet.cssRules;
-    for (let j = 0; j < rules.length; j++) {
-      const rule = rules[j];
 
-      // Check if the rule has a background color property
+    for (const rule of rules) {
       if (rule.style && rule.style.backgroundColor) {
-        oldBgColor = rule.style.backgroundColor;
-        // Replace the rule's background color with the new color
         rule.style.backgroundColor = newBgColor;
       }
       if (rule.style && rule.style.color) {
-        oldTextColor = rule.style.color;
-        // Replace the rule's background color with the new color
         rule.style.color = newTextcolor;
       }
     }
